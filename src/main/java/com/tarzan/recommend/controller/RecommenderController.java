792247bf992c13ca0040d8f5bdf69388b358fe1c
tarzan <liuyapeng@bdtd.com>
2023-03-21 13:46:54 +0800

    简单的web改造

--- src/main/java/com/tarzan/recommend/controller/RecommenderController.java
+++ src/main/java/com/tarzan/recommend/controller/RecommenderController.java
@@ -2,6 +2,7 @@ package com.tarzan.recommend.controller;
 
 import com.tarzan.recommend.recommender.RecommenderFactory;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.lenskit.api.ItemRecommender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,9 @@ public class RecommenderController {
 
 
     @GetMapping("/recommend")
-    public List<Long> getRecommendItem(int userId, int num){
-        ItemRecommender recommender= RecommenderFactory.getItemRecommender("popular");
+    @ApiOperation(value = "物品推荐",notes = "内置的type有 item-item;user-user;popular;slope-one;funk-svd;")
+    public List<Long> getRecommendItem(int userId, int num,String type){
+        ItemRecommender recommender= RecommenderFactory.getItemRecommender(type);
         return recommender.recommend(userId, num, null, null);
     }
 }

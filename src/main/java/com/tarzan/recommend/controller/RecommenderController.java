e3ae26bd97e3ce4418569c84ce73aa45bcf5d999
tarzan <liuyapeng@bdtd.com>
2023-03-21 13:30:31 +0800

    简单的web改造

--- src/main/java/com/tarzan/recommend/controller/RecommenderController.java
+++ src/main/java/com/tarzan/recommend/controller/RecommenderController.java
@@ -1,6 +1,7 @@
 package com.tarzan.recommend.controller;
 
 import com.tarzan.recommend.recommender.RecommenderFactory;
+import io.swagger.annotations.Api;
 import org.lenskit.api.ItemRecommender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,12 @@ import java.util.List;
 /**
  * @author Lenovo
  */
+@Api(tags = "物品推荐接口")
 @RestController
 public class RecommenderController {
 
 
-    @GetMapping("/recommender")
+    @GetMapping("/recommend")
     public List<Long> getRecommendItem(int userId, int num){
         ItemRecommender recommender= RecommenderFactory.getItemRecommender("popular");
         return recommender.recommend(userId, num, null, null);
